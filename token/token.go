ba56101d781b01c6f04be73e7963d28e46621a39
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-09-05 00:32:12 +0900

    add token, add lexer test case

--- token/token.go
+++ token/token.go
@@ -7,3 +7,29 @@ type Token struct {
     Literal string
 }
 
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+
+    // 識別子 + リテラル
+    IDENT = "IDENT"
+    INT = "INT"
+
+    // 演算子
+    ASSIGN = "="
+    PLUS = "+"
+
+    // デリミタ
+    COMMA = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    // キーワード
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+)
+
