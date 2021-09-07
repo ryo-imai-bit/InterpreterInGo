946f8b3826433463ebe1fc04c48408e7f24213f0
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-09-07 14:54:19 +0900

    finish adding lexer, add repl

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+    "fmt"
+    "os"
+    "os/user"
+    "github.com/ryo-imai-bit/InterpreterInGo/repl"
+)
+
 func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("HEllo %s! This is the Monkey programming language!\n", user.Username)
+    fmt.Printf("Feel free to type in commands\n")
+    repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "github.com/ryo-imai-bit/InterpreterInGo/lexer"
+    "github.com/ryo-imai-bit/InterpreterInGo/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+    scanner := bufio.NewScanner(in)
+
+    for {
+        fmt.Printf(PROMPT)
+        scanned := scanner.Scan()
+        if !scanned {
+            return
+        }
+
+        line := scanner.Text()
+        l := lexer.New(line)
+
+        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+            fmt.Printf("%+v\n", tok)
+        }
+    }
+}
