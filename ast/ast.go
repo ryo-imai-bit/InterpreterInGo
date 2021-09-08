5c6cab20ad6dbf99f25f566efbac5496ab1187e1
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-09-09 01:24:19 +0900

    add statements parser, and test

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 package ast
 
 import (
+    "bytes"
+
     "github.com/ryo-imai-bit/InterpreterInGo/token"
 )
 
@@ -9,6 +11,7 @@ import (
 
 type Node interface {
     TokenLiteral() string
+    String() string
 }
 
 type Statement interface {
@@ -32,15 +35,39 @@ func (p *Program) TokenLiteral() string {
         return ""
     }
 }
+func (p *Program) String() string {
+    var out bytes.Buffer
+
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+
+    return out.String()
+}
 
 type LetStatement struct {
     Token token.Token // token.LET
-    Name *Identifier
+    Name *Identifier // 識別子は共有されうる(他のところの識別子と同一のものを扱っている必要がある)からポインタ？
     Value Expression
 }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
 
 type Identifier struct {
     Token token.Token // token.Ident
@@ -49,4 +76,27 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
+
+type ReturnStatement struct {
+    Token token.Token // 'return'
+    ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+    Token token.Token // 式の最初のトークン
+    Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
+}
 
